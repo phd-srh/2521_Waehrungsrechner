7c1157c2e1e2df994f40f22441d6a5415d2c3ab9
Dr. Dillinger <peter.dillinger@srh.de>
2023-07-11 09:21:38 +0200

    Top-Down-Entwurf: Main.main ist soweit fertig, fehlt nur noch die Klasse Währungsrechner (TODO).

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Währungsrechner dollarRechner = new Währungsrechner(0.91);
+
+        Scanner eingabe = new Scanner(System.in);
+        System.out.print("Bitte Betrag eingeben: ");
+        double betrag = eingabe.nextDouble();
+
+        double dollarBetrag = dollarRechner.inFremd(betrag);
+        double euroBetrag   = dollarRechner.inEuro(betrag);
+
+        System.out.println(betrag + " in Euro sind " + dollarBetrag + " Dollar");
+        System.out.println(betrag + " in Dollar sind " + euroBetrag + " Euro");
+    }
 }
